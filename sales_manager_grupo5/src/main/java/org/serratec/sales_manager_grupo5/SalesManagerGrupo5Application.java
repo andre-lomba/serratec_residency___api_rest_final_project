302cd18d311b408c2d47fc2fb58cc042a5043ed2
André Lomba Garcia Roza <andrelombagr@gmail.com>
2023-05-18 17:43:50 -0300

    projeto criado

---
+++ sales_manager_grupo5/src/main/java/org/serratec/sales_manager_grupo5/SalesManagerGrupo5Application.java
@@ -0,0 +1,13 @@
+package org.serratec.sales_manager_grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagerGrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagerGrupo5Application.class, args);
+	}
+
+}
---
+++ sales_manager_grupo5/src/test/java/org/serratec/sales_manager_grupo5/SalesManagerGrupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.sales_manager_grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagerGrupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
